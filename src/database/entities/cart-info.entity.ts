d1f39a7e207ccb4dd733522059ff22de4c2a1bd4
Viktar Belski <Viktar_Belski@epam.com>
2023-05-15 18:05:40 +0300

    remove useless and configure serverless

---
+++ src/database/entities/cart-info.entity.ts
@@ -0,0 +1,13 @@
+// import { Column, Entity, PrimaryColumn } from 'typeorm';
+//
+// @Entity()
+// export class CartInfo {
+//     @PrimaryColumn({type: 'uuid', nullable: false})
+//     cart_id: string;
+//
+//     @Column({ type: 'text', nullable: false, generated: 'uuid' })
+//     product_id: string;
+//
+//     @Column({ type: 'number' })
+//     count: string;
+// }
